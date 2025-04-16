2b4eea517290ada9dfb84e226913e8079eb690c7
giriparus <parusgiri@gmail.com>
2025-04-16 17:23:17 +0530

    stream stdout and stderr to log file

--- script.sh
+++ script.sh
@@ -18,11 +18,11 @@ if ! [ -z ${DB_NAME} ] && ! [  -z ${SERVER} ] ; then
   echo "============"
 
   echo "Step 1. Mysqldump"
-  $CURRENT_DIR/mysql/mysql-backup.sh $MYSQL_USERNAME $MYSQL_PASSWORD $SERVER $DB_NAME $FILE_NAME $FILE_PATH
+  $CURRENT_DIR/mysql/mysql-backup.sh $MYSQL_USERNAME $MYSQL_PASSWORD $SERVER $DB_NAME $FILE_NAME $FILE_PATH >> /var/log/backup.log 2>&1
   echo "Step 2. Saving to S3"
-  $CURRENT_DIR/mysql/backup.sh $FILE_NAME $BUCKET_NAME
+  $CURRENT_DIR/mysql/backup.sh $FILE_NAME $BUCKET_NAME >> /var/log/backup.log 2>&1
   echo "Step 3. Cleaning it up"
-  $CURRENT_DIR/mysql/clean.sh $FILE_NAME
+  $CURRENT_DIR/mysql/clean.sh $FILE_NAME >> /var/log/backup.log 2>&1
   echo "Done MYSQL"
 
 fi;
@@ -33,11 +33,11 @@ if ! [ -z ${MONGODB_URI} ] ; then
   echo "============"
 
   echo "Step 1: Mongodump"
-  bash $CURRENT_DIR/mongodb/mongo-backup.sh
+  bash $CURRENT_DIR/mongodb/mongo-backup.sh >> /var/log/backup.log 2>&1
   echo "Step 2: Saving to S3"
-  bash $CURRENT_DIR/mongodb/s3.sh
+  bash $CURRENT_DIR/mongodb/s3.sh >> /var/log/backup.log 2>&1
   echo "Step 3. Cleaning it up"
-  bash $CURRENT_DIR/mongodb/clean.sh
+  bash $CURRENT_DIR/mongodb/clean.sh >> /var/log/backup.log 2>&1
   echo "Done Mongo"
 
 fi;
